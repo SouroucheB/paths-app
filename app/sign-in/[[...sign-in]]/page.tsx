17ea7c52b7535bbfc65d8548eeaf0ee20fcad440
Sourouche Bagherzadeh <bagherzadeh@ext.lemonde.fr>
2024-06-25 12:17:43 +0200

    change SignIn & SignUp pages name

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignIn } from "@clerk/nextjs";
 
-export default function Login() {
+export default function SignInPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignIn />
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function Login() {
+export default function SignUpPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignUp />
