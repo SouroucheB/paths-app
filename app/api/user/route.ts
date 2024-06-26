af74cdb14f8b9a709f0f5aa3f4980258c287d567
Sourouche Bagherzadeh <bagherzadeh@ext.lemonde.fr>
2024-06-26 16:12:42 +0200

    replace login api route to user

---
+++ app/api/user/route.ts
@@ -0,0 +1,15 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  const user = await currentUser();
+
+  return NextResponse.json({ user: user }, { status: 200 });
+}
